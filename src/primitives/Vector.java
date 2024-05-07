654c507972507079225ecc057e9d1349ae1fcb1c
YehonatanYeret <yyeret@g.jct.ac.il>
2024-05-07 18:00:25 +0300

    implement Vector

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,4 +1,48 @@
 package primitives;
 
-public class Vector {
+public class Vector extends Point {
+    public Vector(double x, double y, double z) {
+        super(x, y, z);
+        if (xyz.equals(Double3.ZERO)) {
+            throw new IllegalArgumentException("Vector cannot be zero vector");
+        }
+    }
+
+    public Vector(Double3 xyz) {
+        super(xyz);
+        if (xyz.equals(Double3.ZERO)) {
+            throw new IllegalArgumentException("Vector cannot be zero vector");
+        }
+    }
+
+    public Vector add(Vector v) {
+        return new Vector(this.xyz.add(v.xyz));
+    }
+
+    public Vector scale(double scalar) {
+        return new Vector(this.xyz.scale(scalar));
+    }
+
+    public double dotProduct(Vector v) {
+        return this.xyz.d1 * v.xyz.d1 + this.xyz.d2 * v.xyz.d2 + this.xyz.d3 * v.xyz.d3;
+    }
+
+    public Vector cross(Vector v) {
+        return new Vector(this.xyz.d2 * v.xyz.d3 - this.xyz.d3 * v.xyz.d2,
+                this.xyz.d3 * v.xyz.d1 - this.xyz.d1 * v.xyz.d3,
+                this.xyz.d1 * v.xyz.d2 - this.xyz.d2 * v.xyz.d1);
+    }
+
+    public double lengthSquared() {
+        return this.dotProduct(this);
+    }
+
+    public double length() {
+        return Math.sqrt(this.lengthSquared());
+    }
+
+    public Vector normalize() {
+        return new Vector(this.xyz.reduce(this.length()));
+    }
+
 }
