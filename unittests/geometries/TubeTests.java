916fdb5be56d0cd289624b43b75be5c9b260cdf8
YehonatanYeret <yyeret@g.jct.ac.il>
2024-05-12 00:43:12 +0300

    Sphere.getNormal

--- unittests/geometries/TubeTests.java
+++ unittests/geometries/TubeTests.java
@@ -1,7 +1,9 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
-
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
