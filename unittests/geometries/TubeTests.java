3780bd143281c196c2ca86a80fdd3b4227a7a4b3
Maor Noy <manoy@g.jct.ac.il>
2024-05-11 22:44:35 +0300

    add tests to tube class

--- unittests/geometries/TubeTests.java
+++ unittests/geometries/TubeTests.java
@@ -1,6 +1,9 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,5 +17,14 @@ class TubeTests {
      */
     @Test
     void testGetNormal() {
+        // ============ Equivalence Partitions Tests ==============
+        // TC01: There is a simple test for the getNormal function
+        Tube tube = new Tube(new Ray(
+                new Point(0, 0, 0),
+                new Vector(0, 0, 1)), 1);
+        assertEquals(new Vector(1, 0, 0), tube.getNormal(new Point(1, 0, 1)), "Bad normal to tube");
+        // =============== Boundary Values Tests =================
+        // TC02: (p-p0) is orthogonal to the axis of the tube
+        assertEquals(new Vector(1, 0, 0), tube.getNormal(new Point(1, 0, 0)), "Bad normal to tube");
     }
 }
