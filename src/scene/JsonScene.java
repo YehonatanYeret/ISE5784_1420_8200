8401fd66c832d5e4d34de43c1a40a8ceb1e2239d
YehonatanYeret <yyeret@g.jct.ac.il>
2024-06-02 22:30:13 +0300

    refactor JsonScene.java

--- src/scene/JsonScene.java
+++ src/scene/JsonScene.java
@@ -1,5 +1,6 @@
 package scene;
 
+import geometries.Geometries;
 import lighting.AmbientLight;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -14,82 +15,104 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * JsonScene class represents a scene in the 3D space
+ * JsonScene class represents a scene in the 3D space and provides methods to import a scene from a JSON file.
  */
 public class JsonScene {
 
     /**
-     * Import a scene from a json file
-     * @param path the path to the json file
-     * @return the scene imported from the json file
+     * Imports a scene from a JSON file.
+     *
+     * @param path the path to the JSON file
+     * @param name the name of the scene
+     * @return the scene imported from the JSON file
+     * @throws IOException if there is an error reading the file
+     * @throws ParseException if there is an error parsing the JSON
      */
     public static Scene importScene(String path, String name) throws IOException, ParseException {
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(path));
         JSONObject sceneObj = (JSONObject) jsonObject.get("scene");
 
-        // Get the background color from the json object
-        String rgb = (String) sceneObj.get("background-color");
-        String[] rgbArr = rgb.split(" ");
-        Color background = new Color(Double.parseDouble(rgbArr[0]), Double.parseDouble(rgbArr[1]), Double.parseDouble(rgbArr[2]));
-
-        // Get the ambient light from the json object
+        Color background = parseColor((String) sceneObj.get("background-color"));
         JSONObject ambientLightObj = (JSONObject) sceneObj.get("ambient-light");
-        rgb = (String) ambientLightObj.get("color");
-        rgbArr = rgb.split(" ");
+        Color ambientLight = parseColor((String) ambientLightObj.get("color"));
+        double ka = ((Number) ambientLightObj.get("ka")).doubleValue();
 
-        AmbientLight ambientLight = new AmbientLight(new Color(Double.parseDouble(rgbArr[0]), Double.parseDouble(rgbArr[1]), Double.parseDouble(rgbArr[2])),(double)(ambientLightObj.get("ka")));
+        Geometries geometries = new Geometries(parseGeometries((JSONArray) sceneObj.get("geometries")).toArray(new Geometry[0]));
 
-        // Get the objects from the json object
-        JSONArray geometries = (JSONArray) sceneObj.get("geometries");
-        List<Geometry> geometryList = new ArrayList<>();
-        for (Object obj : geometries) {
+        return new Scene(name).setGeometries(geometries).setBackground(background).setAmbientLight(new AmbientLight(ambientLight, ka));
+    }
+
+    /**
+     * Parses a color from a string in the format "R G B".
+     *
+     * @param rgb the string representing the color
+     * @return the parsed color
+     */
+    private static Color parseColor(String rgb) {
+        String[] rgbArr = rgb.split(" ");
+        return new Color(Double.parseDouble(rgbArr[0]), Double.parseDouble(rgbArr[1]), Double.parseDouble(rgbArr[2]));
+    }
+
+    /**
+     * Parses a list of geometries from a JSON array.
+     *
+     * @param geometriesArray the JSON array representing geometries
+     * @return the list of parsed geometries
+     */
+    private static List<Geometry> parseGeometries(JSONArray geometriesArray) {
+        List<Geometry> geometries = new ArrayList<>();
+        for (Object obj : geometriesArray) {
             JSONObject geometryObj = (JSONObject) obj;
 
-            if (geometryObj.containsKey("polygon")) {
-                JSONObject polygonObj = (JSONObject) geometryObj.get("polygon");
-                JSONArray verticesArray = (JSONArray) polygonObj.get("vertices");
-                List<Point> vertices = new ArrayList<>();
-                for (Object vertex : verticesArray) {
-                    String[] vertexCoords = ((String) vertex).split(" ");
-                    vertices.add(new Point(
-                            Double.parseDouble(vertexCoords[0]),
-                            Double.parseDouble(vertexCoords[1]),
-                            Double.parseDouble(vertexCoords[2])
-                    ));
-                }
-                geometryList.add(new Polygon(vertices.toArray(new Point[0])));
-            } else if (geometryObj.containsKey("sphere")) {
-                JSONObject sphereObj = (JSONObject) geometryObj.get("sphere");
-                String[] centerCoords = ((String) sphereObj.get("center")).split(" ");
-                Point center = new Point(
-                        Double.parseDouble(centerCoords[0]),
-                        Double.parseDouble(centerCoords[1]),
-                        Double.parseDouble(centerCoords[2])
-                );
-                double radius = Double.parseDouble((String) sphereObj.get("radius"));
-                geometryList.add(new Sphere(radius, center));
-            }else if (geometryObj.containsKey("triangle")) {
-                JSONObject triangleObj = (JSONObject) geometryObj.get("triangle");
-                JSONArray verticesArray = (JSONArray) triangleObj.get("vertices");
-                List<Point> vertices = new ArrayList<>();
-                for (Object vertex : verticesArray) {
-                    String[] vertexCoords = ((String) vertex).split(" ");
-                    vertices.add(new Point(
-                            Double.parseDouble(vertexCoords[0]),
-                            Double.parseDouble(vertexCoords[1]),
-                            Double.parseDouble(vertexCoords[2])
-                    ));
-                }
-                geometryList.add(new Triangle(vertices.get(0), vertices.get(1), vertices.get(2)));
+            if (geometryObj.containsKey("sphere")) {
+                geometries.add(parseSphere((JSONObject) geometryObj.get("sphere")));
+            } else if (geometryObj.containsKey("triangle")) {
+                geometries.add(parseTriangle((JSONObject) geometryObj.get("triangle")));
             }
         }
+        return geometries;
+    }
 
-        // Create and return the scene
-        Scene scene = new Scene(name).setGeometries(new Geometries(geometryList.toArray(new Geometry[0])))
-                .setBackground(background)
-                .setAmbientLight(ambientLight);
+    /**
+     * Parses a sphere from a JSON object.
+     *
+     * @param sphereObj the JSON object representing a sphere
+     * @return the parsed sphere
+     */
+    private static Sphere parseSphere(JSONObject sphereObj) {
+        Point center = parsePoint((String) sphereObj.get("center"));
+        double radius = Double.parseDouble((String) sphereObj.get("radius"));
+        return new Sphere(radius, center);
+    }
 
-        return scene;
+    /**
+     * Parses a triangle (polygon) from a JSON object.
+     *
+     * @param triangleObj the JSON object representing a triangle
+     * @return the parsed triangle
+     */
+    private static Polygon parseTriangle(JSONObject triangleObj) {
+        JSONArray verticesArray = (JSONArray) triangleObj.get("vertices");
+        Point[] vertices = new Point[verticesArray.size()];
+        for (int i = 0; i < verticesArray.size(); i++) {
+            vertices[i] = parsePoint((String) verticesArray.get(i));
+        }
+        return new Polygon(vertices);
+    }
+
+    /**
+     * Parses a point from a string in the format "X Y Z".
+     *
+     * @param pointStr the string representing the point
+     * @return the parsed point
+     */
+    private static Point parsePoint(String pointStr) {
+        String[] coords = pointStr.split(" ");
+        return new Point(
+                Double.parseDouble(coords[0]),
+                Double.parseDouble(coords[1]),
+                Double.parseDouble(coords[2])
+        );
     }
 }
